c20d5ae1b4aca32dd8a7abfc4a57bcc6bb8cefc2
skrusche63 <skrusche63@googlemail.com>
2014-11-30 16:10:18 +0100

    Functionality moved to 'spark-core'.

--- src/main/scala/de/kp/spark/outlier/actor/OutlierIndexer.scala
+++ src/main/scala/de/kp/spark/outlier/actor/OutlierIndexer.scala
@@ -18,134 +18,47 @@ package de.kp.spark.outlier.actor
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.util.Date
-
-import akka.actor.{Actor,ActorLogging}
-
 import de.kp.spark.core.model._
-import de.kp.spark.core.elastic.{ElasticBuilderFactory => EBF}
 
-import de.kp.spark.core.io.ElasticIndexer
+import de.kp.spark.core.actor.BaseIndexer
+import de.kp.spark.outlier.Configuration
 
-import de.kp.spark.outlier.model._
-
-import scala.collection.JavaConversions._
-import scala.collection.mutable.HashMap
-
-class OutlierIndexer extends BaseActor {
-  
-  def receive = {
+class OutlierIndexer extends BaseIndexer(Configuration) {
+ 
+  override def getSpec(req:ServiceRequest):(List[String],List[String]) = {
     
-    case req:ServiceRequest => {
-     
-      val origin = sender    
-      val uid = req.data("uid")
-
-      req.task match {
-
-        case "index:feature" => {
-          
-          try {
-
-            val index   = req.data("index")
-            val mapping = req.data("type")
+    req.task.split(":")(1) match {
+      
+      case "feature" => {
     
-            val (names,types) = fieldspec(req.data)
+        val names = req.data("names").split(",").toList
+        val types = req.data("types").split(",").toList
     
-            val builder = EBF.getBuilder("feature",mapping,names,types)
-            val indexer = new ElasticIndexer()
-        
-            indexer.create(index,mapping,builder)
-            indexer.close()
-      
-            val data = Map("uid" -> uid, "message" -> Messages.SEARCH_INDEX_CREATED(uid))
-            val response = new ServiceResponse(req.service,req.task,data,OutlierStatus.SUCCESS)	
-      
-            val origin = sender
-            origin ! response
-            
-          } catch {
-        
-            case e:Exception => {
-          
-              log.error(e, e.getMessage())
-      
-              val data = Map("uid" -> uid, "message" -> e.getMessage())
-              val response = new ServiceResponse(req.service,req.task,data,OutlierStatus.FAILURE)	
-      
-              val origin = sender
-              origin ! response
-          
-            }
-      
-          } finally {
-        
-            context.stop(self)
-
-          }
-          
-        }
-        
-        case "index:sequence" => {
-          
-          try {
-            
-            val index   = req.data("index")
-            val mapping = req.data("type")
-    
-            val builder = EBF.getBuilder("product",mapping)
-            val indexer = new ElasticIndexer()
-      
-            val data = Map("uid" -> uid, "message" -> Messages.SEARCH_INDEX_CREATED(uid))
-            val response = new ServiceResponse(req.service,req.task,data,OutlierStatus.SUCCESS)	
-      
-            val origin = sender
-            origin ! response
-            
-          } catch {
-        
-            case e:Exception => {
-          
-              log.error(e, e.getMessage())
-      
-              val data = Map("uid" -> uid, "message" -> e.getMessage())
-              val response = new ServiceResponse(req.service,req.task,data,OutlierStatus.FAILURE)	
-      
-              val origin = sender
-              origin ! response
-          
-            }
-      
-          } finally {
-        
-            context.stop(self)
-
-          }
-          
-        }
-        
-        case _ => {
-          
-          val msg = Messages.TASK_IS_UNKNOWN(uid,req.task)
-          
-          origin ! failure(req,msg)
-          context.stop(self)
-          
-        }
+        (names,types)
         
       }
-
+      case "sequence" => {
+        
+        (List.empty[String],List.empty[String])
+        
+      }
+      
+      case _ => throw new Exception("Unknown topic.")
+      
     }
-    
-  }
- 
-  private def fieldspec(params:Map[String,String]):(List[String],List[String]) = {
-
-    val names = params("names").split(",").toList
-    val types = params("types").split(",").toList
-    
-    (names,types)
     
   }  
  
+  override def getTopic(req:ServiceRequest):String = {
+    
+    req.task.split(":")(1) match {
+      
+      case "feature" => "feature"
+      case "sequence" => "product"
+      
+      case _ => throw new Exception("Unknown topic.")
+    }
+    
+  }
+
 }
